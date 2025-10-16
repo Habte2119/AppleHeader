96ece5ac66b843e29285378176e17540fb50651b
habte2119 <habtemariam81@gmail.com>
2025-10-16 12:28:39 +0300

    first commit

---
+++ MyApp/src/App.jsx
@@ -0,0 +1,24 @@
+import React, {Component} from 'react';
+
+import Nav from './Components/Nav/Nav'
+// import Footer from './Components/Footer/Footer'
+// import Main from './Components/Main/Main'
+
+
+
+import './App.css';
+
+class App extends Component {
+	render(){
+	  return (
+	    <div>
+	      <Nav /> 
+	      {/* <Main /> 
+	      <Footer />  */}
+	    </div>
+	  )		
+	}
+
+}
+
+export default App
